c163938e0f013465ddf0eac1771f7f34487a0498
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-10 15:46:04 -0400

    First Commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './Button.css';
+
+export default function Button() {
+    const [clicked, setClicked] = useState(false);
+
+    function onPress() {
+        setClicked(true);
+        setTimeout(() => setClicked(false), 100);
+    }
+
+    return (
+        <button className={clicked ? 'clicked' : null} onClick={onPress}>
+            Schedule Call
+        </button>
+    );
+}
