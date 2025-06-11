8df5596b63eeaf1429e2d4e255501196708acb84
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-11 11:12:16 -0400

    WIP browser router, nav menu modal

---
+++ src/components/NavModal.jsx
@@ -0,0 +1,15 @@
+import '/NavModal.css';
+
+export default function NavModal() {
+    return (
+        <div>
+            <header>Company Menu</header>
+            <hr></hr>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+        </div>
+    );
+}
