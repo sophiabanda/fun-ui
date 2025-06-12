8c7e4445e40d66bd73caee31528d4cd6bf1b5791
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-12 19:20:44 -0400

    unique button press

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,16 @@
 import './Card.css';
+import '../index.css';
 import products from '../../data';
+import { useState } from 'react';
 
 export default function Card() {
+    const [clickedId, setClickedId] = useState(null);
+
+    function onButtonClick(id) {
+        setClickedId(id);
+        setTimeout(() => setClickedId(null), 100);
+    }
+
     return (
         <>
             <div className="card-container">
@@ -11,7 +20,15 @@ export default function Card() {
                         <article>{product.description}</article>
                         <div className="price-button-div">
                             <p>Price: {product.price}</p>
-                            <button id="cart-button">Add to Cart</button>
+                            <button
+                                id="cart-button"
+                                onClick={() => onButtonClick(product.id)}
+                                className={
+                                    clickedId === product.id ? 'clicked' : ''
+                                }
+                            >
+                                Add to Cart
+                            </button>
                         </div>
                     </div>
                 ))}
